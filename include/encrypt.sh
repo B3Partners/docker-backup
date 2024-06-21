98129d2b11296615c3cd3684873267805d95b3a5
Matthijs Laan <matthijsln@gmail.com>
2024-06-21 15:00:12 +0200

    Fix indent; remove superfluous comments; log filename only; fix "boolean" expressions; exit when encryption fails

--- include/encrypt.sh
+++ include/encrypt.sh
@@ -2,41 +2,39 @@
 # This script uses gpg to encrypt files
 
 encrypt_files() {
-    if [[ $ENCRYPT != "true" ]]; then
-        return
+  if [[ $ENCRYPT != "true" ]]; then
+    return
+  fi
+  mkdir -p $(dirname $KEYFILE)
+  touch $KEYFILE
+
+  echo "-----BEGIN PGP PUBLIC KEY BLOCK-----" > $KEYFILE
+  echo "                                    " >> $KEYFILE
+  echo "${PUBLIC_KEY}" >> $KEYFILE
+  echo "-----END PGP PUBLIC KEY BLOCK----" >> $KEYFILE
+
+
+  encrypt_dir=${DIR_BACKUP}
+
+  success="true"
+  start_time=$(date +"%s")
+
+  for file in "${encrypt_dir}"/*; do
+    print_msg "Encrypting file: $(basename $file)"
+    gpg --quiet --output "${file}.gpg" --encrypt --recipient-file "${KEYFILE}" "${file}"
+    if [[ $? -ne 0 ]]; then
+      success="false"
+      break
     fi
-    mkdir -p $(dirname $KEYFILE)
-    touch $KEYFILE
+  done
 
-    echo "-----BEGIN PGP PUBLIC KEY BLOCK-----" > $KEYFILE
-    echo "                                    " >> $KEYFILE
-    echo "${PUBLIC_KEY}" >> $KEYFILE
-    echo "-----END PGP PUBLIC KEY BLOCK----" >> $KEYFILE
+  end_time=$(date +"%s")
 
-
-    encrypt_dir=${DIR_BACKUP}
-
-    success=true  # assume success initially
-
-    start_time=$(date +"%s")   # record the current time in seconds since epoch
-
-    for file in "${encrypt_dir}"/*; do
-        print_msg "encrypting file: ${file} "
-        gpg --quiet --output "${file}.gpg" --encrypt --recipient-file "${KEYFILE}" "${file}"
-        if [ $? -ne 0 ]; then  # check the exit status of the last command
-            success=false
-            break
-        fi
-    done
-
-    end_time=$(date +"%s")  # record the current time in seconds since epoch after encryption is complete
-
-    if $success; then
-        elapsed_time=$((end_time - start_time))  # calculate the difference between the two times
-        print_msg "Encryption succeeded and took ${elapsed_time} seconds"
-        find $DIR_BACKUP/ -type f ! \( -name "*.gpg" -o -name "*.keep" -o -name "*.sha256" \) -exec rm {} \;
-        else
-        print_error "Encryption failed causing back-up failure."
-    fi
-
+  if [[ $success = "true" ]]; then
+    elapsed_time=$((end_time - start_time))
+    print_msg "Encryption succeeded and took ${elapsed_time} seconds"
+    find $DIR_BACKUP/ -type f ! \( -name "*.gpg" -o -name "*.keep" -o -name "*.sha256" \) -exec rm {} \;
+  else
+    print_error_and_exit "Encryption failed, invalid key specified? Unencrypted backup remains on system, but not uploading unencrypted backup to SFTP"
+  fi
 }
